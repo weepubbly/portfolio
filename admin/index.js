f8a9def2a31ed36c59f7f7ecbd6e9656e021a8bc
pubbly <122093334+weepubbly@users.noreply.github.com>
2023-01-19 23:18:13 +0900

    Add files via upload

--- admin/index.js
+++ admin/index.js
@@ -1,3 +1,4 @@
+
 let info = [];
     $(function(){
         $('#save').click(function(){
@@ -6,9 +7,9 @@ let info = [];
             let engname = $('#engname').val();
             let start = $('#start').val();
             let dob = $('#dob').val();
-            let idnum = $('#idnum').val();
             let phone = $('#phone').val();
             let email = $('#email').val();
+            let statement = $('#statement').val();
             let index = info.length;
             info.push([]);
             info[index].push(membernum);
@@ -16,15 +17,41 @@ let info = [];
             info[index].push(engname);
             info[index].push(start);
             info[index].push(dob);
-            info[index].push(idnum);
             info[index].push(phone);
             info[index].push(email);
+            info[index].push(statement);
 
-            let ul="<ul>";
+            let ul="";
             $.each(info, function(index, element){
                 ul+="<li><p>"+element[0]+"</p><p>"+element[1]+"</p><p>"+element[2]+"</p><p>"+element[3]+"</p><p>"+element[4]+"</p><p>"+element[5]+"</p><p>"+element[6]+"</p><p>"+element[7]+"</p></li>";
             });
-            ul += "</ul>";
-            $('#content').html(ul);
+            $(ul).appendTo('.information');
         });
     });
+
+
+    $('#content').empty();
+    $.ajax({
+        url:'list.json',
+        dataType:'json',
+        success:function(d){
+            let str = $('<ul class = "information">');
+            for(let i  in d.DATA){
+                let row = $('<li />').append(
+                    $('<p />').text(d.DATA[i].membernum),
+                    $('<p />').text(d.DATA[i].name), 
+                    $('<p />').text(d.DATA[i].engname), 
+                    $('<p />').text(d.DATA[i].start), 
+                    $('<p />').text(d.DATA[i].dob), 
+                    $('<p />').text(d.DATA[i].phone),
+                    $('<p />').text(d.DATA[i].email),
+                    $('<p />').text(d.DATA[i].statement) 
+                );
+                str.append(row);
+            }
+            $('#content').append(str);
+        },
+        error:function(e){
+            console.log(e);
+        }
+    });    
